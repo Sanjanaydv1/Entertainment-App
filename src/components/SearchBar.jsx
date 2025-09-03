ac7b72c9ad483c1486e6c0ed9c811bd7ea184966
Sanjana Yadav <126410251+SanjanaYdv1@users.noreply.github.com>
2025-09-03 20:09:01 +0545

    updated

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,19 @@
 
+import { FiSearch } from 'react-icons/fi';
+
 function SearchBar({ placeholder, onSearch }) {
   return (
     <form className="search-bar" onSubmit={(e) => {
       e.preventDefault();
       onSearch(e.target.elements.query.value);
+      e.target.reset();
     }}>
-      <button type="submit">🔍</button> 
       <input
         type="text"
         name="query"
         placeholder={placeholder}
       />
+      <button type="submit"><FiSearch className="w-5 h-5" /></button>
       
     </form>
   );
